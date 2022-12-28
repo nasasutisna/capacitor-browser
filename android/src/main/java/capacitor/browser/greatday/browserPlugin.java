28c690643f1d36c1bf754cb1fbfaad48adfe22f8
Nasa Sutisna <nasa.sutisna@greatdayhr.com>
2022-12-28 10:08:22 +0700

    update custom browser

--- android/src/main/java/capacitor/browser/greatday/browserPlugin.java
+++ android/src/main/java/capacitor/browser/greatday/browserPlugin.java
@@ -1,22 +1,90 @@
 package capacitor.browser.greatday;
 
-import com.getcapacitor.JSObject;
+
+import android.content.ActivityNotFoundException;
+import android.net.Uri;
+import com.getcapacitor.Logger;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
+import com.getcapacitor.util.WebColor;
 
-@CapacitorPlugin(name = "browser")
-public class browserPlugin extends Plugin {
+@CapacitorPlugin(name = "Browser")
+public class BrowserPlugin extends Plugin {
 
-    private browser implementation = new browser();
+    private Browser implementation;
+
+    public void load() {
+        implementation = new Browser(getContext());
+        implementation.setBrowserEventListener(this::onBrowserEvent);
+    }
 
     @PluginMethod
-    public void echo(PluginCall call) {
-        String value = call.getString("value");
+    public void open(PluginCall call) {
+        // get the URL
+        String urlString = call.getString("url");
+        if (urlString == null) {
+            call.reject("Must provide a URL to open");
+            return;
+        }
+        if (urlString.isEmpty()) {
+            call.reject("URL must not be empty");
+            return;
+        }
+        Uri url;
+        try {
+            url = Uri.parse(urlString);
+        } catch (Exception ex) {
+            call.reject(ex.getLocalizedMessage());
+            return;
+        }
 
-        JSObject ret = new JSObject();
-        ret.put("value", implementation.echo(value));
-        call.resolve(ret);
+        // get the toolbar color, if provided
+        String colorString = call.getString("toolbarColor");
+        Integer toolbarColor = null;
+        if (colorString != null) try {
+            toolbarColor = WebColor.parseColor(colorString);
+        } catch (IllegalArgumentException ex) {
+            Logger.error(getLogTag(), "Invalid color provided for toolbarColor. Using default", null);
+        }
+
+        // open the browser and finish
+        try {
+            implementation.open(url, toolbarColor);
+        } catch (ActivityNotFoundException ex) {
+            Logger.error(getLogTag(), ex.getLocalizedMessage(), null);
+            call.reject("Unable to display URL");
+            return;
+        }
+        call.resolve();
+    }
+
+    @PluginMethod
+    public void close(PluginCall call) {
+        call.unimplemented();
+    }
+
+    @Override
+    protected void handleOnResume() {
+        if (!implementation.bindService()) {
+            Logger.error(getLogTag(), "Error binding to custom tabs service", null);
+        }
+    }
+
+    @Override
+    protected void handleOnPause() {
+        implementation.unbindService();
+    }
+
+    void onBrowserEvent(int event) {
+        switch (event) {
+            case Browser.BROWSER_LOADED:
+                notifyListeners("browserPageLoaded", null);
+                break;
+            case Browser.BROWSER_FINISHED:
+                notifyListeners("browserFinished", null);
+                break;
+        }
     }
 }
--- ios/Plugin/browserPlugin.m
+++ ios/Plugin/browserPlugin.m
@@ -1,8 +1,8 @@
 #import <Foundation/Foundation.h>
 #import <Capacitor/Capacitor.h>
 
-// Define the plugin using the CAP_PLUGIN Macro, and
-// each method the plugin supports using the CAP_PLUGIN_METHOD macro.
-CAP_PLUGIN(browserPlugin, "browser",
-           CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+CAP_PLUGIN(CAPBrowserPlugin, "Browser",
+  CAP_PLUGIN_METHOD(open, CAPPluginReturnPromise);
+  CAP_PLUGIN_METHOD(close, CAPPluginReturnPromise);
+  CAP_PLUGIN_METHOD(removeAllListeners, CAPPluginReturnPromise);
 )
